e0c0369d2d896ab0a87e4802b16fa3e75420f80c
Denton Elliott <dendo74@hotmail.com>
2024-05-28 23:31:32 -0400

    render template support

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -9,24 +11,36 @@ import (
 
 const port = ":4000"
 
-type application struct {}
+type application struct {
+	templateCache map[string]*template.Template
+	config        appConfig
+}
+
+type appConfig struct {
+	useCache bool
+}
 
 func main() {
-    app := application{}
+	app := application{
+	    templateCache: make(map[string]*template.Template),
+	}
 
-    srv := &http.Server{
-        Addr: port,
-        Handler: app.routes(),
-        IdleTimeout: 30 * time.Second,
-        ReadTimeout: 30 * time.Second,
-        ReadHeaderTimeout: 30 * time.Second,
-        WriteTimeout: 30 * time.Second,
-    }
+	flag.BoolVar(&app.config.useCache, "cache", false, "Use template cache")
+	flag.Parse()
 
-    fmt.Println("Starting web application on port", port)
-    
-    err := srv.ListenAndServe()
-    if err != nil {
-        log.Fatal(err)
-    }
+	srv := &http.Server{
+		Addr:              port,
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
+
+	fmt.Println("Starting web application on port", port)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
