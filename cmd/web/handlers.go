9733ce1aa7a9ec17272d4c8cc71e8487f9f33b08
Denton Elliott <dendo74@hotmail.com>
2024-05-31 16:02:49 -0400

    default pages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"slices"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func (app * application) ShowHome(w http.ResponseWriter, r *http.Request) {
+var pages = []string {
+    "home",
+    "about",
+    "cat-breed",
+    "cat-breeds",
+    "cat-breeders",
+    "dog-breed",
+    "dog-breeds",
+    "dog-breeders",
+}
+
+func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
     app.render(w, "home.page.gohtml", nil)
+}
+
+func (app *application) ShowPage(w http.ResponseWriter, r *http.Request) {
+    page := chi.URLParam(r, "page")
+    if !slices.Contains(pages, page) {
+        fmt.Printf("Requested page: %s is not a valid page.\n", page)
+        return
+    }
+    app.render(w, fmt.Sprintf("%s.page.gohtml", page), nil)
 }
