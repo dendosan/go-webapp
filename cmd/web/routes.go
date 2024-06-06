9cd7df83f365efa9e26a62a9734a4db2571e115c
Denton Elliott <dendo74@hotmail.com>
2024-06-06 19:05:15 -0400

    factory pattern
    * test page for factory pattern
    
    * abstract factory
    
    * test html for abstract factory

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,15 @@ func (app *application) routes() http.Handler {
     fileServer := http.FileServer(http.Dir("./static/"))
     mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
+    // display test page
+    mux.Get("/test-patterns", app.TestPatterns)
+
+    // factory routes
+    mux.Get("/api/dog-from-factory", app.CreateDogFromFactory)
+    mux.Get("/api/cat-from-factory", app.CreateCatFromFactory)
+    mux.Get("/api/dog-from-abstract-factory", app.CreateDogFromAbstractFactory)
+    mux.Get("/api/cat-from-abstract-factory", app.CreateCatFromAbstractFactory)
+
     mux.Get("/", app.ShowHome)
     mux.Get("/{page}", app.ShowPage)
 
