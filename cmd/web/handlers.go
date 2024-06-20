9d9f35c89e4be4ba005c525ac7e57b1a86daee70
Denton Elliott <dendo74@hotmail.com>
2024-06-20 19:30:39 -0400

    adapter for cat breeds

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,113 +10,124 @@ import (
 	"github.com/tsawler/toolbox"
 )
 
-var pages = []string {
-    "test",
-    "home",
-    "about",
-    "cat-breed",
-    "cat-breeds",
-    "cat-breeders",
-    "dog-breed",
-    "dog-breeds",
-    "dog-breeders",
+var pages = []string{
+	"test",
+	"home",
+	"about",
+	"cat-breed",
+	"cat-breeds",
+	"cat-breeders",
+	"dog-breed",
+	"dog-breeds",
+	"dog-breeders",
 }
 
 func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
-    app.render(w, "home.page.gohtml", nil)
+	app.render(w, "home.page.gohtml", nil)
 }
 
 func (app *application) ShowPage(w http.ResponseWriter, r *http.Request) {
-    page := chi.URLParam(r, "page")
-    if !slices.Contains(pages, page) {
-        fmt.Printf("Requested page: %s is not a valid page.\n", page)
-        return
-    }
-    app.render(w, fmt.Sprintf("%s.page.gohtml", page), nil)
+	page := chi.URLParam(r, "page")
+	if !slices.Contains(pages, page) {
+		fmt.Printf("Requested page: %s is not a valid page.\n", page)
+		return
+	}
+	app.render(w, fmt.Sprintf("%s.page.gohtml", page), nil)
 }
 
 func (app *application) CreateDogFromFactory(w http.ResponseWriter, r *http.Request) {
-    var t toolbox.Tools
-    _ = t.WriteJSON(w, http.StatusOK, pets.NewPet("dog"))
+	var t toolbox.Tools
+	_ = t.WriteJSON(w, http.StatusOK, pets.NewPet("dog"))
 }
 func (app *application) CreateCatFromFactory(w http.ResponseWriter, r *http.Request) {
-    var t toolbox.Tools
-    _ = t.WriteJSON(w, http.StatusOK, pets.NewPet("cat"))
+	var t toolbox.Tools
+	_ = t.WriteJSON(w, http.StatusOK, pets.NewPet("cat"))
 }
 
-func (app *application) TestPatterns (w http.ResponseWriter, r *http.Request) {
-    app.render(w, "test.page.gohtml", nil)
+func (app *application) TestPatterns(w http.ResponseWriter, r *http.Request) {
+	app.render(w, "test.page.gohtml", nil)
 }
 
-func (app *application) CreateDogFromAbstractFactory (w http.ResponseWriter, r *http.Request) {
-    var t toolbox.Tools
-    dog, err := pets.NewPetFromAbstractFactory("dog")
-    if err != nil {
-        _ = t.ErrorJSON(w, err, http.StatusBadRequest)
-        return
-    }
-    _ = t.WriteJSON(w, http.StatusOK, dog)
+func (app *application) CreateDogFromAbstractFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	dog, err := pets.NewPetFromAbstractFactory("dog")
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, dog)
 }
 
-func (app *application) CreateCatFromAbstractFactory (w http.ResponseWriter, r *http.Request) {
-    var t toolbox.Tools
-    cat, err := pets.NewPetFromAbstractFactory("cat")
-    if err != nil {
-        _ = t.ErrorJSON(w, err, http.StatusBadRequest)
-        return
-    }
-    _ = t.WriteJSON(w, http.StatusOK, cat)
+func (app *application) CreateCatFromAbstractFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	cat, err := pets.NewPetFromAbstractFactory("cat")
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, cat)
 }
 
 func (app *application) GetAllDogBreedsJSON(w http.ResponseWriter, r *http.Request) {
-    dogBreeds, err := app.App.Models.DogBreed.All()
-    var t toolbox.Tools
+	dogBreeds, err := app.App.Models.DogBreed.All()
+	var t toolbox.Tools
 
-    if err != nil {
-        _ = t.ErrorJSON(w, err, http.StatusBadRequest)
-        return
-    }
-    _ = t.WriteJSON(w, http.StatusOK, dogBreeds)
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, dogBreeds)
 }
 
 func (app *application) CreateDogWithBuilder(w http.ResponseWriter, r *http.Request) {
-    var t toolbox.Tools
+	var t toolbox.Tools
 
-    // create a dog using the builder pattern
-    p, err := pets.NewPetBuilder().
-        SetSpecies("dog").
-        SetBreed("mixed breed").
-        SetWeight(15).
-        SetDescription("A mixed breed of unknown origin.  Probably has some spaniel heritage.").
-        SetColor("Black and White").
-        SetAge(3).
-        SetAgeEstimated(true).
-        Build() 
+	// create a dog using the builder pattern
+	p, err := pets.NewPetBuilder().
+		SetSpecies("dog").
+		SetBreed("mixed breed").
+		SetWeight(15).
+		SetDescription("A mixed breed of unknown origin.  Probably has some spaniel heritage.").
+		SetColor("Black and White").
+		SetAge(3).
+		SetAgeEstimated(true).
+		Build()
 
-    if err != nil {
-        _ = t.ErrorJSON(w, err, http.StatusBadRequest)
-    }
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+	}
 
-    _ = t.WriteJSON(w, http.StatusOK, p)
+	_ = t.WriteJSON(w, http.StatusOK, p)
 }
 
 func (app *application) CreateCatWithBuilder(w http.ResponseWriter, r *http.Request) {
-    var t toolbox.Tools
+	var t toolbox.Tools
 
-    // create a cat using the builder pattern
-    p, err := pets.NewPetBuilder().
-        SetSpecies("cat").
-        SetBreed("calico").
-        SetWeight(7).
-        SetDescription("Calico cats are known for their unique tri-color coat pattern, which typically consists of three main colors: white, black, and orange").
-        SetColor("white, black, orange").
-        SetAge(1).
-        SetAgeEstimated(true).
-        Build() 
+	// create a cat using the builder pattern
+	p, err := pets.NewPetBuilder().
+		SetSpecies("cat").
+		SetBreed("calico").
+		SetWeight(7).
+		SetDescription("Calico cats are known for their unique tri-color coat pattern, which typically consists of three main colors: white, black, and orange").
+		SetColor("white, black, orange").
+		SetAge(1).
+		SetAgeEstimated(true).
+		Build()
 
-    if err != nil {
-        _ = t.ErrorJSON(w, err, http.StatusBadRequest)
-    }
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+	}
 
-    _ = t.WriteJSON(w, http.StatusOK, p)
+	_ = t.WriteJSON(w, http.StatusOK, p)
+}
+
+func (app *application) GetCatBreeds(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+
+	breeds, err := app.catService.GetAllBreeds()
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+	}
+
+	_ = t.WriteJSON(w, http.StatusOK, breeds)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,6 +16,7 @@ type application struct {
 	templateCache map[string]*template.Template
 	config        appConfig
 	App           *configuration.Application
+	catService    *RemoteService
 }
 
 type appConfig struct {
@@ -38,7 +39,11 @@ func main() {
 		log.Panic(err)
 	}
 
-    app.App = configuration.New(dbPool)
+	jsonBackend := &JSONBackend{}
+	jsonAdapter := &RemoteService{Remote: jsonBackend}
+
+	app.App = configuration.New(dbPool)
+	app.catService = jsonAdapter
 
 	srv := &http.Server{
 		Addr:              port,
